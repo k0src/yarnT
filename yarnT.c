6fcc21dda476199b9fe9fa92cdf2d73416f8366a
k0 <k0src@null.net>
2024-05-11 17:51:40 -0500

    die

--- yarnT.c
+++ yarnT.c
@@ -3,16 +3,27 @@
 #include <termios.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <errno.h>
 
 struct termios orig_termios;
 
+void die(const char *s) 
+{
+    perror(s);
+    exit(1);
+}
+
 void disableRawMode() 
 {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
 }
 
 void enableRawMode() 
 {
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
+
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disableRawMode);
 
@@ -26,17 +37,20 @@ void enableRawMode()
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
 }
 
 int main() 
 {
     enableRawMode();
 
-    char c = 'x';
+    char c = '\0';
     while (1) {
         char c = '\0';
-        read(STDIN_FILENO, &c, 1);
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+            die("read");
+
         if (iscntrl(c)) {
             printf("%d\r\n", c);
         }
