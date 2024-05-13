a5dacdf6591acd3a00baa7310ddb70c90b0227fb
k0 <k0src@null.net>
2024-05-12 20:32:21 -0500

    adding rows kinda works

--- src/yarnT.c
+++ src/yarnT.c
@@ -41,7 +41,7 @@ struct editorConfig
     int screen_rows;
     int screen_cols;
     int numrows;
-    erow row;
+    erow* row;
     struct termios orig_termios; 
 };
 
@@ -190,6 +190,18 @@ int getWindowSize(int* rows, int* cols)
     }
 }
 
+void editorAppendRow(char* s, size_t len) 
+{
+    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+
+    int at = E.numrows;
+    E.row[at].size = len;
+    E.row[at].chars = malloc(len + 1);
+    memcpy(E.row[at].chars, s, len);
+    E.row[at].chars[len] = '\0';
+    E.numrows++;
+}
+
 // file i/o
 void editorOpen(char* filename) 
 {
@@ -202,14 +214,10 @@ void editorOpen(char* filename)
     ssize_t len;
 
     linelen = getline(&line, &linelen, fp);
-    if (linelen != -1) {
+    while ((len = getline(&line, &linelen, fp)) != -1) {
         while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
             linelen--;
-        E.row.size = linelen;
-        E.row.chars = malloc(linelen + 1);
-        memcpy(E.row.chars, line, linelen);
-        E.row.chars[linelen] = '\0';
-        E.numrows = 1;
+        editorAppendRow(line, linelen);
     }
     free(line);
     fclose(fp);
@@ -331,10 +339,10 @@ void editorDrawRows(struct abuf *ab)
         }
     }
     else {
-        int len = E.row.size;
+        int len = E.row[y].size;
         if (len > E.screen_cols)
             len = E.screen_cols;
-        abAppend(ab, E.row.chars, len);
+        abAppend(ab, E.row[y].chars, len);
     }
 
     abAppend(ab, "\x1b[K", 3);
@@ -368,6 +376,7 @@ void initEditor() {
     E.c_y = 0;
 
     E.numrows = 0;
+    E.row = NULL;
 
     if (getWindowSize(&E.screen_rows, &E.screen_cols) == -1)
         die("getWindowSize");
